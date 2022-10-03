0a8758b26b492b38d77d78539088d336f71fa161
Marek 'seqre' Grzelak <git@seqre.dev>
2022-10-02 20:22:39 -0400

    [feat] MVP of agent and C2 (#1, #2, #3)
    It's a mess, but it's worth keeping as a MVP and baseline.

--- rast-agent/src/main.rs
+++ rast-agent/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpStream;
+use std::io::{Read, Write};
+
+fn main() -> std::io::Result<()> {
+    println!("Hello from client!");
+
+    let mut stream = TcpStream::connect("127.0.0.1:42069")?;
+
+    let msg = b"Ping!";
+    stream.write(msg)?;
+
+    let mut buf = [0; 128];
+    match stream.read(&mut buf) {
+        Ok(bytes_read) => {
+            if bytes_read > 0 {
+                println!("{}", String::from_utf8_lossy(&buf));
+            }
+        },
+        Err(e) => println!("Error: {e:?}"),
+    }
+
+    Ok(())
 }
--- rast-c2/src/main.rs
+++ rast-c2/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 128];
+
+    match stream.read(&mut buf) {
+        Ok(bytes_read) => {
+            if bytes_read > 0 {
+                println!("{}", String::from_utf8_lossy(&buf));
+            }
+        },
+        Err(e) => println!("Error: {e:?}"),
+    }
+
+    let msg = b"Pong!";
+    stream.write(msg).unwrap();
+}
+
+fn main() -> std::io::Result<()> {
+    println!("Hello from server!");
+    
+    let listener = TcpListener::bind("127.0.0.1:42069")?;
+
+    match listener.accept() {
+        Ok((stream, addr)) => {
+            println!("Got connection from: {addr:?}");
+            handle_client(stream);
+        },
+        Err(e) => println!("Error: {e:?}"),
+    }
+    Ok(())
 }

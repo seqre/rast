bc73bd85cbbca3e274b107defe4f8c29f9998b77
Marek 'seqre' Grzelak <git@seqre.dev>
2022-10-02 20:28:33 -0400

    [chore] clippy & fmt

--- rast-agent/src/main.rs
+++ rast-agent/src/main.rs
@@ -1,5 +1,7 @@
-use std::net::TcpStream;
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
 
 fn main() -> std::io::Result<()> {
     println!("Hello from client!");
@@ -7,7 +9,7 @@ fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:42069")?;
 
     let msg = b"Ping!";
-    stream.write(msg)?;
+    let _ = stream.write(msg)?;
 
     let mut buf = [0; 128];
     match stream.read(&mut buf) {
@@ -15,7 +17,7 @@ fn main() -> std::io::Result<()> {
             if bytes_read > 0 {
                 println!("{}", String::from_utf8_lossy(&buf));
             }
-        },
+        }
         Err(e) => println!("Error: {e:?}"),
     }
 
--- rast-c2/src/main.rs
+++ rast-c2/src/main.rs
@@ -1,5 +1,7 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn handle_client(mut stream: TcpStream) {
     let mut buf = [0; 128];
@@ -9,24 +11,24 @@ fn handle_client(mut stream: TcpStream) {
             if bytes_read > 0 {
                 println!("{}", String::from_utf8_lossy(&buf));
             }
-        },
+        }
         Err(e) => println!("Error: {e:?}"),
     }
 
     let msg = b"Pong!";
-    stream.write(msg).unwrap();
+    let _ = stream.write(msg).unwrap();
 }
 
 fn main() -> std::io::Result<()> {
     println!("Hello from server!");
-    
+
     let listener = TcpListener::bind("127.0.0.1:42069")?;
 
     match listener.accept() {
         Ok((stream, addr)) => {
             println!("Got connection from: {addr:?}");
             handle_client(stream);
-        },
+        }
         Err(e) => println!("Error: {e:?}"),
     }
     Ok(())

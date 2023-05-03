d6545dab14d5ed6c8ea67de8b7edf995c1fae688
Marek 'seqre' Grzelak <git@seqre.dev>
2023-05-03 06:15:39 -0400

    [docs] documentation of encoding.rs

--- rast/src/encoding.rs
+++ rast/src/encoding.rs
@@ -1,13 +1,17 @@
+//! Abstraction over \[en|de]coding of data
+
 use bytes::Bytes;
 use serde::{Deserialize, Serialize};
 
 use crate::Result;
 
+/// Abstraction over \[en|de]coding of data
 pub trait Packager {
     fn encode<T: Serialize>(&self, data: &T) -> Result<Bytes>;
     fn decode<'de, T: Deserialize<'de>>(&self, bytes: &'de Bytes) -> Result<T>;
 }
 
+/// JSON \[en|de]coder for data
 #[derive(Debug, Default)]
 pub struct JsonPackager;
 

24bc92c71843958f14432a4c0a083f43a7651ed3
Marek 'seqre' Grzelak <git@seqre.dev>
2023-05-03 02:00:05 -0400

    [feat] improve protocol client/server creations

--- rast-cli/src/main.rs
+++ rast-cli/src/main.rs
@@ -1,10 +1,35 @@
-use anyhow::anyhow;
+use std::sync::Arc;
+
+use anyhow::{anyhow, bail, Result};
+use futures_util::{SinkExt, StreamExt};
 use rast::{
-    protocols::{tcp::TcpFactory, ProtoFactory},
-    settings::Settings,
+    encoding::{JsonPackager, Packager},
+    messages::ui_request::{UiRequest, UiResponse},
+    protocols::{quic::QuicFactory, tcp::TcpFactory, Messager, ProtoConnection, ProtoFactory},
+    settings::{Connection, Settings},
     RastError,
 };
 use rast_cli::{get_shell, ShellState};
+use tokio::sync::Mutex;
+use tracing::{debug, info};
+use tracing_subscriber::filter::LevelFilter;
+
+async fn get_connection(settings: &Settings) -> Result<Arc<Mutex<dyn ProtoConnection>>> {
+    for conf in settings.server.ui_listeners.iter() {
+        let conn = match conf {
+            Connection::Tcp(tcp_conf) => TcpFactory::new_client(tcp_conf).await,
+            Connection::Quic(quic_conf) => QuicFactory::new_client(quic_conf).await,
+            _ => bail!(RastError::Unknown),
+        };
+
+        if let Ok(conn) = conn {
+            debug!("Got connection: {conn:?}");
+            return Ok(conn);
+        }
+    }
+
+    Err(RastError::Unknown.into())
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), RastError> {
@@ -23,12 +48,7 @@ async fn main() -> Result<(), RastError> {
         },
     };
 
-    let connection = if let Some(conf) = &conf.server.ui {
-        let conf = &conf.tcp.unwrap();
-        TcpFactory::new_client(conf).await
-    } else {
-        Err(anyhow!("Can't connect to C2 using TCP.").into())
-    };
+    let connection = get_connection(&conf).await?;
 
     let state = ShellState::new(connection.unwrap());
 

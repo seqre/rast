c008e25bd3e40ace438ad80a6144615f1c8e2f45
Marek 'seqre' Grzelak <git@seqre.dev>
2023-05-03 02:07:01 -0400

    [feat] add whoami command

---
+++ rast-agent/src/commands/perms.rs
@@ -0,0 +1,43 @@
+use std::{
+    ffi::OsString,
+    sync::{Arc, RwLock},
+};
+
+use async_trait::async_trait;
+
+use crate::{
+    commands::{Command, CommandCategory, CommandOutput},
+    context::Context,
+};
+
+/// Get username of owner of the process.
+#[derive(Default)]
+pub struct Whoami;
+
+#[async_trait]
+impl Command for Whoami {
+    fn get_name(&self) -> &'static str {
+        "whoami"
+    }
+
+    fn get_short_desc(&self) -> &'static str {
+        "get process owner's username"
+    }
+
+    fn get_options(&self) -> Vec<(&'static str, &'static str)> {
+        vec![]
+    }
+
+    fn get_category(&self) -> CommandCategory {
+        CommandCategory::Permissions
+    }
+
+    async fn execute(
+        &self,
+        ctx: Arc<RwLock<Context>>,
+        args: Vec<String>,
+    ) -> anyhow::Result<CommandOutput> {
+        let output = format!("{}@{}", whoami::username(), whoami::hostname());
+        Ok(CommandOutput::Text(output))
+    }
+}
